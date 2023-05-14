8d8dd64ff95d7d15f4f75a8a9dedc6bc8e4e131a
Coderlancce <coderlancce@gmail.com>
2023-05-14 10:21:54 -0500

    OK Prime numbers & timing execution

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,7 +1,33 @@
-function countPrimeNumbers() {
-    return 25;
+// Prime numbers & timing execution
+
+function countPrimeNumbers(){
+    let numOfPrimeNums = 0
+
+    function isPrime(number) {
+        colect = 1;
+        if (number < 2) {
+            colect = 0;
+        }
+        else {
+            for (let i = 2; i < number; i++) {
+                if (number % i === 0) {
+                    colect = 0;
+                }
+            }
+        }
+        return colect
+    }
+
+    for (let num = 2; num < 101; num++) {
+        if (isPrime(num) == 1) {
+            numOfPrimeNums += 1
+        }
+    }
+
+    return numOfPrimeNums;
 }
-const init = performance.now()
+
+let a = performance.now();
 countPrimeNumbers();
-const end = performance.now()
-console.log(`Execution time of printing countPrimeNumbers was ${end - init} milliseconds.`);
+let b = performance.now();
+console.log('Execution time of printing countPrimeNumbers was ' + (b - a) + ' milliseconds.');
